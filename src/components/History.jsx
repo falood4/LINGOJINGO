3764bf7f4cb4b333f718ffa7cdc5257ccc22fae7
Kurien <kuriennakkara0@gmail.com>
2025-06-17 15:51:12 +0530

    vocab and js

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -2,16 +2,24 @@ function History() {
     return (
         <div className="card-history-card">
             <h3>History</h3>
-            <p><h6>You have no attempts Press start to begin</h6></p>
+            <h6 style={{ opacity: 0.5 }}>You have no attempts. Press START to begin</h6>
             <div className="history-list">
                 <div className="card-button-secondary">
                     <div>
                         <p>
                             Started
                         </p>
-                        <h6>
+                        <h3>
                             14.02.25
-                        </h6>
+                        </h3>
+                    </div>
+                    <div>
+                        <p>
+                            Streak
+                        </p>
+                        <h3>
+                            5
+                        </h3>
                     </div>
                 </div>
             </div>
